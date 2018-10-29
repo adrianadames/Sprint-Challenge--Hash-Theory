abacbdf96e26cf3e9b6944f97ff9f42d5d32e849
Adrian Adames <adrianadames@gmail.com>
2018-10-29 09:28:28 -0400

    reconstruct_trip function in progress.

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,15 @@
 def reconstruct_trip(tickets):
-  pass 
+  tickets_dict = {}
+  route = [''] * len(tickets)-1
+  for ticket in tickets:
+    tickets_dict[ticket[0]] = ticket[1]
+    if ticket[0] == None:
+      route[0] = ticket[1]
+  
+  for i in range(1, len(tickets)-1):
+    route[i] = route[i-1]
+
+  
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
