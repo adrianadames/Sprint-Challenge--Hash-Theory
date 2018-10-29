83b3960fee39010bfd2eb28e9041878bf66313c1
Adrian Adames <adrianadames@gmail.com>
2018-10-29 06:36:36 -0400

    Added problem statement as a comment.

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,3 +1,11 @@
+# Given a package with a weight limit limit and a list weights of item weights, 
+# implement a function get_indices_of_item_weights that finds two items whose 
+# sum of weights equals the weight limit limit. Your function should return a 
+# tuple (i, j) of the indices of the item weights, ordered such that i > j. If 
+# such a pair doesn’t exist, return an empty tuple.
+
+# Your solution should run in linear time.
+
 def get_indices_of_item_weights(weights, limit):
   pass 
 
